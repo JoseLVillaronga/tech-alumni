0d8d7394de77a1efa136a974ca1fc8e46476e476
JoseLVillaronga <149181653+JoseLVillaronga@users.noreply.github.com>
2023-10-27 12:36:55 -0300

    Update Encripta.php

--- clases/Encripta.php
+++ clases/Encripta.php
@@ -5,7 +5,7 @@
  */
 class Encripta{
  
-    private static $Key = "TeCcAm365";
+    private static $Key = "A0l1u2m3n4i5";
  
     public static function encrypt ($input) {
         $output = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5(Encripta::$Key), $input, MCRYPT_MODE_CBC, md5(md5(Encripta::$Key))));
